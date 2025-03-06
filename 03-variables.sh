d25f28180729ce6f4e3db0d051c5d468d7976b1f
bandarukavya <bandarukavya22@gmail.com>
2025-03-06 19:13:00 +0530

    First Commit

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# declaring variable
+person1=Ramesh
+person2=Suresh
+
+# refering a variable
+echo "$person1:: Hello $person2, How are you?"
+echo "$person2:: Hi $person1, I am fine. How are you?"
+echo "$person1: I am fine too. how is your work?"
+echo "$person2:: not bad. I am thinking to upgrade to DevOps"
