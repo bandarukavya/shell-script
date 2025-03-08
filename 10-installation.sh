f7df1bfadaa6ae485b9e3220b6c214de23734f54
bandarukavya <bandarukavya22@gmail.com>
2025-03-08 20:40:18 +0530

    commit prog

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+else
+    echo "You are super user."
+fi
+
+
 dnf install mysql -y
