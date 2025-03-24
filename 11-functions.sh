547947a9a1f06308bee26ea80348556dbe1a869d
bandarukavya <bandarukavya22@gmail.com>
2025-03-24 21:11:58 +0530

    Fuctions

---
+++ 11-functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "You are super user."
+fi
+
+VALIDATE(){
+    echo "Exit Status: $1"
+    echo "What are you doing: $2"
+}
