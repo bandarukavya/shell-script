6e2b8898eb6bb748ebdbe3bb9f2bd8735198ca32
bandarukavya <bandarukavya22@gmail.com>
2025-03-25 12:46:20 +0530

    Colours

--- 12-colors.sh
+++ 12-colors.sh
@@ -20,12 +20,12 @@ else
 fi
 
 VALIDATE(){
-    if[ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2... $G SUCCESS $N"
+        echo -e "$2... $G SUCCESS $N"
     fi
 }
 
