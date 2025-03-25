651f2a938303cd94174b799c36a604c844c0dfc1
bandarukavya <bandarukavya22@gmail.com>
2025-03-25 12:45:10 +0530

    Colours

---
+++ 12-colors.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "Script started execiting at: $TIMESTAMP"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with user access"
+else
+    echo "You are super user"
+fi
+
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo "$2... $G SUCCESS $N"
+    fi
+}
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MYSQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing GIT"
+
+dnf install dockerr -y &>>$LOGFILE
+VALIDATE $? "Installing Docker"
